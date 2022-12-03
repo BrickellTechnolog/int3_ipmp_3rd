ee40f2d919b766f4882ff96b0247dd9dba98392b
BrickellTechnolog <43421542+BrickellTechnolog@users.noreply.github.com>
2022-12-02 20:47:19 -0500

    3rd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>Int3rPlanetary MarketPlace 3</title>
+        <title>thirdweb Marketplace with Next.JS</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
